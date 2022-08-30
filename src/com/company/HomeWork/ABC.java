6e9925f16515e8fe227c37787ea167f1d3c3e7c1
IlyaRF <ilyagaitanov@gmail.com>
2022-08-30 23:14:26 +0300

    HomeWork 3

---
+++ src/com/company/HomeWork/ABC.java
@@ -0,0 +1,62 @@
+package com.company.HomeWork;
+
+public class ABC {
+
+
+        static final Object object = new Object();
+        static String currentLetter = "A";
+
+        public static void main(String[] args) {
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        synchronized (object) {
+                            while (!currentLetter.equals("A")) {
+                                object.wait();
+                            }
+                            System.out.print(currentLetter);
+                            currentLetter = "B";
+                            object.notifyAll();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        synchronized (object) {
+                            while (!currentLetter.equals("B")) {
+                                object.wait();
+                            }
+                            System.out.print(currentLetter);
+                            currentLetter = "C";
+                            object.notifyAll();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        synchronized (object) {
+                            while (!currentLetter.equals("C")) {
+                                object.wait();
+                            }
+                            System.out.print(currentLetter);
+                            currentLetter = "A";
+                            object.notifyAll();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+
